21948f3fb46fd130c90a2b2f416d3c5f64d473b6
Erick <erickluques@gmail.com>
2020-01-26 17:07:06 -0200

    basic properties

--- src/Model.php
+++ src/Model.php
@@ -3,7 +3,13 @@
 namespace ErickFirmo;
 
 abstract class Model {
-    
+    protected $paginate = false;
+    protected $limit = false;
+    protected $cascade = false;
+    protected $action = NULL;
+    public $pivot_entity = NULL;
+    public $pivot_parent_id = NULL;
+    public $pivot_table = NULL;
 }
 
 
